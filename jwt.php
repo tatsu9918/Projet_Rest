e0d97262bc355c3e7e3b7cb6e7e11d6234c58a38
Guillaume <guillaume.pomies82@gmail.com>
2023-03-31 08:31:28 +0200

    retest

--- jwt.php
+++ jwt.php
@@ -15,40 +15,40 @@ $mySqlConnection = "mysql:host=" . $host . ";dbname=" . $dbname;
 
 $linkpdo = connectionBDD($mySqlConnection, $user, $pwd);
 if ($http_method == "POST") {
-//     //Vérifie si l'utilisateur à bien rentré ses logins et mot de passes dans l'UR
-//     if (!empty($_GET['login']) && !empty($_GET['password'])) {
-//         $req = $linkpdo->prepare('SELECT Libellé FROM role
-//         INNER JOIN utilisateur
-//         ON role.Id_Role = utilisateur.Id_Role
-//         WHERE utilisateur.nom = :nom AND utilisateur.mdp = :mdp;');
+    //Vérifie si l'utilisateur à bien rentré ses logins et mot de passes dans l'UR
+    if (!empty($_GET['login']) && !empty($_GET['password'])) {
+        $req = $linkpdo->prepare('SELECT Libellé FROM role
+        INNER JOIN utilisateur
+        ON role.Id_Role = utilisateur.Id_Role
+        WHERE utilisateur.nom = :nom AND utilisateur.mdp = :mdp;');
 
-//         if ($req == false) {
-//             die('Error preparation');
-//         }
+        if ($req == false) {
+            die('Error preparation');
+        }
 
-//         $req2 = $req->execute(
-//             array(
-//                 "nom" => $_GET['login'],
-//                 "mdp" => $_GET['password']
-//             )
-//         );
+        $req2 = $req->execute(
+            array(
+                "nom" => $_GET['login'],
+                "mdp" => $_GET['password']
+            )
+        );
 
-//         if ($req2 == false) {
-//             $req->DebugDumpParams();
-//             die('Error execute');
-//         }
+        if ($req2 == false) {
+            $req->DebugDumpParams();
+            die('Error execute');
+        }
 
-//         $matchingData = $req->fetchAll();
+        $matchingData = $req->fetchAll();
 
-//         //print_r($matchingData[0][0]);
+        //print_r($matchingData[0][0]);
 
-//         //Si l'utilisateur n'est pas enregistré
-//         if (count($matchingData) == 0) {
-//             /// Envoi de la réponse au Client
-//             deliver_response(401, "401 Opération refusée : Votre compte n'est pas enregistré dans notre base. Vous continurez en tant que non Authentifé", NULL);
-//         }
-//         //Si l'utilisateur n'est pas Publisher
-//         else {
+        //Si l'utilisateur n'est pas enregistré
+        if (count($matchingData) == 0) {
+            /// Envoi de la réponse au Client
+            deliver_response(401, "401 Opération refusée : Votre compte n'est pas enregistré dans notre base. Vous continurez en tant que non Authentifé", NULL);
+        }
+        //Si l'utilisateur n'est pas Publisher
+        else {
             $username = $_GET['login'];
             $password = $_GET['password'];
             $role = $matchingData[0][0];
@@ -57,10 +57,10 @@ if ($http_method == "POST") {
             $jwt = generate_jwt($headers, $payload);
             echo json_encode(array('token' => $jwt));
             deliver_response(201, "Token bien généré !", $jwt);
-        // }
+        }
 
-            // } else {
-            //     deliver_response(403, "Erreur le token n'est pas généré",NULL);
+            } else {
+                deliver_response(403, "Erreur le token n'est pas généré",NULL);
             }
 
     if ($http_method == "GET") {
@@ -73,7 +73,7 @@ if ($http_method == "POST") {
                 } else {
                     deliver_response(402, "Le Token JWT est invalide",$validity);
                 }
-
+            }
 }
 function deliver_response($status, $status_message, $data)
 {
